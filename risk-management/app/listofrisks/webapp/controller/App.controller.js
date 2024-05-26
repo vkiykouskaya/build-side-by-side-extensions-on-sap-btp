918fd61a75e974f547084f6692075451bd19dd9c
Veranika Kiykouskaya <veranika.kiykouskaya@leverx.com>
2024-05-26 07:18:19 +0000

    Unit 3 fix for ListOfRisks app

--- risk-management/app/listofrisks/webapp/controller/App.controller.js
+++ risk-management/app/listofrisks/webapp/controller/App.controller.js
@@ -5,7 +5,7 @@ sap.ui.define(
     function(BaseController) {
       "use strict";
   
-      return BaseController.extend("listofrisks.listofrisks.controller.App", {
+      return BaseController.extend("listofrisks.controller.App", {
         onInit: function() {
         }
       });
